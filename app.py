690e8653f019cbe2566943056611256ce1b32a52
Jakeoid <jake.triumphant@gmail.com>
2017-05-27 19:54:07 +1000

    Actual Initial commit
    Containing the original website.

---
+++ app.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+# ┌┐ ┬┬─┐┌┐
+# ├┴┐│├┬┘├┴┐
+# └─┘┴┴└─└─┘
+#
+# RANDOM BIRB SITE
+# :copyright: (c) 2017 Jakeoid
+# :license: MIT, see LICENSE.md for details.
+'''Create minecraft-like achievement pictures'''
+
+# FLASK
+from flask import Flask, jsonify, render_template, send_from_directory
+# RANDOM
+from random import randint
+# OS
+from os import walk
+
+# ############################
+
+# Flask APP
+app = Flask(__name__)
+
+# Storing our files.
+global f
+
+# Make our files an empty list.
+f = []
+
+# Run through all the files in the img folder.
+for (dirpath, dirnames, filenames) in walk('img/'):
+	# Get all the filenames
+    f.extend(filenames)
+    # Beep
+    break
+
+# ############################
+
+
+# Image Hoster.
+@app.route('/img/<path:filename>')
+def image(filename):
+	""" Serves the images of the /img/ and other folders """
+    return send_from_directory('./img/', filename)
+
+
+# Site Index.
+@app.route('/')
+def index():
+	""" Hosts the homepage of the website (API DOCUMENTATION) """
+    return render_template('index.html')
+
+
+# API Endpoint.
+@app.route('/tweet', methods=['GET'])
+def tweet():
+	""" Hosts the API in which you can get random birbs from. """
+    url = randint(0, len(f) - 1)
+    return jsonify({'file': f[url]})
+
+# ############################
+
+if __name__ == '__main__':
+    app.run()
---
+++ img/README.md
@@ -0,0 +1,3 @@
+# Image Folder
+
+This folder is empty for good reason! Here is where you want to place the images of what you want to host. Of course we dont want to reflect these on Github, yet, we still want to have the folder generated there i've left this folder empty.
