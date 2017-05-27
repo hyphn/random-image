4bf478447135690d44fe7a4901b63020973abb29
Jakeoid <jake.triumphant@gmail.com>
2017-05-27 21:03:30 +1000

    Fixing smaller errors.
    - Removed the comment relating to Minecraft, was a placeholder, didn't mean to commit it.
    - Moved to tabs for identation of document, wasn't working otherwise in my server.

--- app.py
+++ app.py
@@ -6,7 +6,6 @@
 # RANDOM BIRB SITE
 # :copyright: (c) 2017 Jakeoid
 # :license: MIT, see LICENSE.md for details.
-'''Create minecraft-like achievement pictures'''
 
 # FLASK
 from flask import Flask, jsonify, render_template, send_from_directory
@@ -28,7 +27,7 @@ f = []
 
 # Run through all the files in the img folder.
 for (dirpath, dirnames, filenames) in walk('img/'):
-	# Get all the filenames
+    # Get all the filenames
     f.extend(filenames)
     # Beep
     break
@@ -39,21 +38,21 @@ for (dirpath, dirnames, filenames) in walk('img/'):
 # Image Hoster.
 @app.route('/img/<path:filename>')
 def image(filename):
-	""" Serves the images of the /img/ and other folders """
+    """ Serves the images of the /img/ and other folders """
     return send_from_directory('./img/', filename)
 
 
 # Site Index.
 @app.route('/')
 def index():
-	""" Hosts the homepage of the website (API DOCUMENTATION) """
+    """ Hosts the homepage of the website (API DOCUMENTATION) """
     return render_template('index.html')
 
 
 # API Endpoint.
 @app.route('/tweet', methods=['GET'])
 def tweet():
-	""" Hosts the API in which you can get random birbs from. """
+    """ Hosts the API in which you can get random birbs from. """
     url = randint(0, len(f) - 1)
     return jsonify({'file': f[url]})
 
