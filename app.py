6459d0dd9c584c1fe8491f7b230f5c37b08d388b
Jakeoid <jake.triumphant@gmail.com>
2017-05-30 18:26:47 +1000

    Rewrite + New Endpoints
    - Fixed a few minor css junk.
    - Rewrote the app.py to use Kyoukai.
    - Changed the requirements.txt.
    - Added /tweet.json/ endpoint.
    - Added /tweet/random endpoint.

--- app.py
+++ app.py
@@ -1,62 +1,87 @@
-# -*- coding: utf-8 -*-
-# ┌┐ ┬┬─┐┌┐
-# ├┴┐│├┬┘├┴┐
-# └─┘┴┴└─└─┘
-#
-# RANDOM BIRB SITE
-# :copyright: (c) 2017 Jakeoid
-# :license: MIT, see LICENSE.md for details.
+"""
 
-# FLASK
-from flask import Flask, jsonify, render_template, send_from_directory
+RANDOM BIRB SITE.
+
+:copyright: (c) 2017 Jakeoid.
+:license: MIT, see LICENSE.md for details.
+
+"""
+
+# KYOUKAI
+from kyoukai import Kyoukai
+from kyoukai import util
 # RANDOM
 from random import randint
 # OS
-from os import walk
+from os import walk, path
+# JSON
+import json
 
 # ############################
 
-# Flask APP
-app = Flask(__name__)
+app = Kyoukai(__name__)
 
 # Storing our files.
-global f
+global files
 
 # Make our files an empty list.
-f = []
+files = []
 
 # Run through all the files in the img folder.
 for (dirpath, dirnames, filenames) in walk('img/'):
     # Get all the filenames
-    f.extend(filenames)
+    files.extend(filenames)
     # Beep
     break
 
 # ############################
 
 
-# Image Hoster.
-@app.route('/img/<path:filename>')
-def image(filename):
-    """ Serves the images of the /img/ and other folders """
-    return send_from_directory('./img/', filename)
-
-
 # Site Index.
-@app.route('/')
-def index():
-    """ Hosts the homepage of the website (API DOCUMENTATION) """
-    return render_template('index.html')
+@app.route("/")
+async def index(ctx):
+    """Display the homepage of the website."""
+    with open("templates/index.html") as file:
+        return util.as_html(file.read())
 
 
-# API Endpoint.
-@app.route('/tweet', methods=['GET'])
-def tweet():
-    """ Hosts the API in which you can get random birbs from. """
-    url = randint(0, len(f) - 1)
-    return jsonify({'file': f[url]})
+# Text API Endpoint.
+@app.route('/tweet/')
+async def tweet(ctx):
+    """Host the API in which you can get random birbs from."""
+    url = randint(0, len(files) - 1)
+    return util.as_html("%s" % files[url])
+
+
+# JSON API Endpoint.
+@app.route('/tweet.json/')
+async def tweetjson(ctx):
+    """Host the API in which you can get random birbs from."""
+    url = randint(0, len(files) - 1)
+    return json.dumps({'file': files[url]})
+
+
+# Text API Endpoint.
+@app.route('/tweet/random')
+async def tweetrandom(ctx):
+    """Host the API in which you can get random birbs from."""
+    url = randint(0, len(files) - 1)
+    return util.as_html('<meta http-equiv="refresh" content="0 url=http://birb.pw/img/%s" />' % files[url])
+
+
+# Image Hoster.
+@app.route('/img/<filename>')
+async def image(ctx, filename):
+    """Serve the images of the /img/ and other folders."""
+    location = 'img/' + filename
+
+    with open(location, mode='rb') as file:
+        stream = file.read()
+
+    return stream
+
 
 # ############################
 
-if __name__ == '__main__':
-    app.run()
+# Run our App.
+app.run(ip="127.0.0.1", port=5000)
